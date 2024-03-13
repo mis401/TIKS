3da56c163cca6b08ddb3dd1b50c88e03d9d60fe1
mis401 <62945435+mis401@users.noreply.github.com>
2024-03-14 00:35:23 +0100

    prva migracija sa entitetima

---
+++ EventDriven/event-driven-backend/event-driven-backend/Controllers/EventController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace event_driven_backend.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EventDriven/event-driven-backend/event-driven-backend/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace event_driven_backend.Models
+{
+    public class User
+    {
+        [Key]
+        public required int ID { get; set; }
+        public required string Name { get; set; }
+        public required string Surname { get; set; }
+        public required string Email { get; set; }
+
+        public List<Community> CreatedCommunities { get; set; } = new List<Community>();
+        //public List<Community> Communities { get; set; } M:N
+        //lista zajednica m:n, lista svojih zajendica koje je kreirao
+    }
+}
