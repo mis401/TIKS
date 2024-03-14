12c7a44518904ecf524192b3368586446eac2a67
Andjelija <mijajlovic.andjelija11@gmail.com>
2024-03-14 01:09:58 +0100

    front

--- EventDriven/event-driven-frontend/src/App.tsx
+++ EventDriven/event-driven-frontend/src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom'
 import './App.css';
-import Welcome from './components/Welcome';
 import Auth from './components/Auth';
 import Home from './components/Home';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={ <Welcome /> } />
-      <Route path='auth' element={ <Auth /> } />
+      <Route path='/' element={ <Auth /> } />
       <Route path='/home' element= { <Home /> } /> 
     </Routes>
   );
