a974e5b22c6d0c60cb5090b3700ed89f3dcf02c1
mis401 <62945435+mis401@users.noreply.github.com>
2024-03-16 22:00:22 +0100

    migrations

--- EventDriven/event-driven-backend/event-driven-backend/Migrations/20240316205950_v2.Designer.cs
+++ EventDriven/event-driven-backend/event-driven-backend/Migrations/20240316205950_v2.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using event_driven_backend.Models;
@@ -11,9 +12,11 @@ using event_driven_backend.Models;
 namespace event_driven_backend.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20240316205950_v2")]
+    partial class v2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EventDriven/event-driven-backend/event-driven-backend/Migrations/20240316205950_v2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace event_driven_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class v2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Communities",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(2024, 3, 16, 20, 59, 50, 542, DateTimeKind.Utc).AddTicks(2239),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTime(2024, 3, 16, 13, 54, 33, 481, DateTimeKind.Utc).AddTicks(3770));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Code",
+                table: "Communities",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Code",
+                table: "Communities");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Communities",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(2024, 3, 16, 13, 54, 33, 481, DateTimeKind.Utc).AddTicks(3770),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValue: new DateTime(2024, 3, 16, 20, 59, 50, 542, DateTimeKind.Utc).AddTicks(2239));
+        }
+    }
+}
